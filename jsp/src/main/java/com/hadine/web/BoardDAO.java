33067cf5e110a3c67e54c0d2d61d35b1cb43ba87
hjsong96 <ghkwlsdbwls@gmail.com>
2023-08-08 20:17:53 +0900

    update 8/8

---
+++ jsp/src/main/java/com/hadine/web/BoardDAO.java
@@ -0,0 +1,12 @@
+package com.hadine.web;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+
+
+@Repository
+@Mapper
+public interface BoardDAO {
+	public Integer toSize(); 
+}
